c04976375ffef39a0cb4e473c52f1351462baca9
Starry Knight <whiteh4tter@gmail.com>
2024-08-28 10:43:11 -0400

    adding state hooks for changing the word and definition input fields

--- json-importer/src/App.js
+++ json-importer/src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 import Sheet from '@mui/joy/Sheet';
 import Input from '@mui/joy/Input';
 import Typography from '@mui/joy/Typography';
+import Textarea from '@mui/joy/Textarea';
+
+
 
 function App() {
+
+  const [word, changeWord] = React.useState("");
+  const [def, changeDef] = React.useState("");
+
   return (
-    <Sheet>
-      <Input></Input>
-      <Sheet></Sheet>
+    <Sheet sx={{ display: "flex", width: "100%", height: "100dvh", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <Sheet sx={{ display: "flex", alignItems: "flex-start", flexDirection: "column", width: "80%" }}>
+        <Typography level="h4">Word:</Typography>
+        <Textarea sx={{ width: "30%", height: "10%" }}></Textarea>
+        <Typography level="h4">Definition:</Typography>
+        <Textarea sx={{ width: "50%", height: "20%" }}></Textarea>
+        <Sheet>
+          <Typography level="h4">Output:</Typography>
+          <Textarea value={
+            { word }
+          }></Textarea>
+        </Sheet>
+      </Sheet>
     </Sheet>
   );
 }
